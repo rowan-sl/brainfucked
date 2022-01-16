910432860c2bfc26194ad936fca319310da0d7c9
Rowan S-L <rowan.with.cats@gmail.com>
2022-01-16 11:45:12 -0500

    working, minimal configuration

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::{io::{self, Read, Write}, num::IntErrorKind};
+
+
+struct Interpreter<const MS: usize> {
+    prog: Vec<char>,
+    mem: [u8; MS],
+    dp: usize,
+    ip: usize,
+    stdin: io::Stdin,
+    stdout: io::Stdout,
+}
+
+impl<const MS: usize> Interpreter<MS> {
+    pub fn new(prog: String) -> Self {
+        Self {
+            prog: prog.chars().filter(
+                |ch| {
+                    match ch {
+                        '>' => true,
+                        '<' => true,
+                        '+' => true,
+                        '-' => true,
+                        '.' => true,
+                        ',' => true,
+                        ']' => true,
+                        '[' => true,
+                        _ => false,
+                    }
+                }
+            ).collect::<Vec<char>>(),
+            mem: [0; MS],
+            dp: 0,
+            ip: 0,
+            stdin: io::stdin(),
+            stdout: io::stdout(),
+        }
+    }
+
+    pub fn cycle<const EOF: u8>(&mut self) -> bool {
+        if self.ip == self.prog.len() {
+            return  true;//done
+        } else {
+            let ins = self.prog[self.ip];
+            // dbg!("running instruction {}", ins);
+            match ins {
+                '>' => {
+                    self.dp += 1;
+                }
+                '<' => {
+                    self.dp -= 1;
+                }
+                '+' => {
+                    self.mem[self.dp] += 1;
+                }
+                '-' => {
+                    self.mem[self.dp] -= 1;
+                }
+                '.' => {
+                    self.stdout.write_all(&[self.mem[self.dp]]).unwrap();
+                }
+                ',' => {
+                    let mut input: [u8; 1] = [0; 1];
+                    if let Ok(_) = self.stdin.read_exact(&mut input) {
+                        self.mem[self.dp] = input[0];
+                    } else {
+                        self.mem[self.dp] = EOF;
+                    }
+                }
+                '[' => {
+                    if self.mem[self.dp] == 0 {
+                        loop {
+                            self.ip += 1;
+                            if self.prog[self.ip] == ']' {
+                                break;
+                            }
+                        }
+                    }
+                }
+                ']' => {
+                    if self.mem[self.dp] != 0 {
+                        loop {
+                            self.ip -= 1;
+                            if self.prog[self.ip] == '[' {
+                                break;
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+            self.ip += 1;
+            // dbg!("instruction ptr at {}", self.ip);
+            false
+        }
+    }
+}
+
+
+
+
+
+fn main() {
+    let mut interp: Interpreter<30_000> = Interpreter::new(
+    ",[.,]".to_string()
+    );
+    while !interp.cycle::<0>() {}
+}
